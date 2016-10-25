51bee8f83f4a35030305f6a26933f27676497755
TommyCabrelli <cab.tommy@gmail.com>
2016-10-25 15:03:11 +0200

    Modif Fonction/classe ImportFile

---
+++ src/tp_graph/Engine.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_graph;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Epulapp
+ */
+public class Engine {
+    
+     public ArrayList<String> importFile(String Path) {
+        
+        ArrayList<String> list = new ArrayList();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(Path));
+            String line = br.readLine();
+
+            while (line != null) {
+                list.add(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TP_Graph.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TP_Graph.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            try {
+                br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return list;
+    }
+}
--- src/tp_graph/TP_Graph.java
+++ src/tp_graph/TP_Graph.java
@@ -5,13 +5,6 @@
  */
 package tp_graph;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -23,28 +16,6 @@ public class TP_Graph {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
     }
-
-    public ArrayList<String> importFile(String path) {
-
-        ArrayList<String> list = new ArrayList();
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(path));
-            String line = br.readLine();
-
-            while (line != null) {
-                list.add(line);
-                line = br.readLine();
-            }
-            br.close();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(TP_Graph.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(TP_Graph.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return list;
-    }
-
 }
