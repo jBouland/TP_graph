108959ea22e1ec5f2af6dc4ec7ed9929a63a40c0
Epulapp <joris.bouland@gmail.com>
2016-10-25 16:19:36 +0200

    Merge origin/master
    Conflicts:
    	src/tp_graph/Mot.java
    	src/tp_graph/TP_Graph.java

--- src/tp_graph/Mot.java
+++ src/tp_graph/Mot.java
@@ -1,68 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp_graph;
-
-import java.util.ArrayList;
-
-/**
- *
- * @author Epulapp
- */
-public class Mot {
-
-    private String valeur;
-    private ArrayList<Mot> listeVoisins;
-
-    public Mot(String valeur) {
-        this.valeur = valeur;
-        this.listeVoisins = new ArrayList();
-    }
-    
-    public int getNumberOfVoisins(){
-        return listeVoisins.size();
-    }
-
-    public String getValeur() {
-        return valeur;
-    }
-
-    public void addVoisin(Mot m) {
-        this.listeVoisins.add(m);
-    }
-
-    public ArrayList<Mot> getListeVoisins() {
-        return listeVoisins;
-    }
-    
-
-    public boolean testLien(Mot m) {
-        int numberDiff = 0;
-        if (m.getValeur().length() == this.valeur.length()) {
-            for (int i = 0; i < valeur.length(); i++) {
-                if (valeur.charAt(i) != m.getValeur().charAt(i)) {
-                    numberDiff++;
-                    if (numberDiff > 1) {
-                        return false; //Pas de lien
-                    }
-                }
-            }
-            //On créé un lien
-            this.listeVoisins.add(m);
-            m.addVoisin(this);
-            return true;
-        }
-        return false;
-    }
-
-}
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tp_graph;
 
 import java.util.ArrayList;
@@ -89,6 +24,16 @@ public class Mot {
     public void addVoisin(Mot m) {
         this.listeVoisins.add(m);
     }
+    
+    public int getNumberOfVoisins(){
+        return listeVoisins.size();
+    }
+
+    public ArrayList<Mot> getListeVoisins() {
+        return listeVoisins;
+    }
+    
+    
 
     public boolean testLien(Mot m) {
         int numberDiff = 0;
--- src/tp_graph/TP_Graph.java
+++ src/tp_graph/TP_Graph.java
@@ -8,7 +8,6 @@ package tp_graph;
 import java.io.File;
 import java.util.ArrayList;
 
-
 /**
  *
  * @author Epulapp
@@ -25,86 +24,47 @@ public class TP_Graph {
         Engine e = new Engine();
         Mot m;
         String filePath = new File("").getAbsolutePath();
-        filePath = filePath.concat("\\mots04lettres.txt"); 
+        filePath = filePath.concat("\\mots04lettres.txt");
         System.out.println(filePath);
         ArrayList listResult = e.importFile(filePath);
         //Nombre de sommets : 
         System.out.println("Nombre de sommets : " + listResult.size());
-        
-        for(int i=0; i<listResult.size(); i++){
-            m = (Mot)listResult.get(i);           
-            for(int j=i+1; j<listResult.size(); j++){
-                if(m.testLien((Mot)listResult.get(j))){
+
+        for (int i = 0; i < listResult.size(); i++) {
+            m = (Mot) listResult.get(i);
+            for (int j = i + 1; j < listResult.size(); j++) {
+                if (m.testLien((Mot) listResult.get(j))) {
                     //Pour voir les liens se faire, décommentez la ligne ci-dessous : 
-                   // System.out.println(m.getValeur() + " proche de " + ((Mot)listResult.get(j)).getValeur());
+                    // System.out.println(m.getValeur() + " proche de " + ((Mot)listResult.get(j)).getValeur());
                     nbLiens++;
                 }
             }
-            if(m.getNumberOfVoisins() == 0){
+            if (m.getNumberOfVoisins() == 0) {
                 nbMotsSansVoisins++;
-            } else if(m.getNumberOfVoisins() == 1){
+            } else if (m.getNumberOfVoisins() == 1) {
                 listMot1Voisin.add(m);
             }
         }
-        System.out.println("Nombre d'arêtes : "+ nbLiens);
-        System.out.println("Nombre de mots sans voisin : "+ nbMotsSansVoisins);
+        System.out.println("Nombre d'arêtes : " + nbLiens);
+        System.out.println("Nombre de mots sans voisin : " + nbMotsSansVoisins);
         //Question 5 - On vérifie si les voisins des mots avec un seul voisin ont aussi un seul voisin
-        for(int i=0; i< listMot1Voisin.size(); i++){
-            if(listMot1Voisin.get(i).getListeVoisins().get(0).getNumberOfVoisins() == 1){
-                
-            }
-        }
-        
-    }
-}
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tp_graph;
+        for (int i = 0; i < listMot1Voisin.size(); i++) {
+            if (listMot1Voisin.get(i).getListeVoisins().get(0).getNumberOfVoisins() == 1) {
 
-import java.io.File;
-import java.util.ArrayList;
-
-
-/**
- *
- * @author Epulapp
- */
-public class TP_Graph {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Engine e = new Engine();
-        Mot m;
-        String filePath = new File("").getAbsolutePath();
-        filePath = filePath.concat("\\mots04lettres.txt"); 
-        System.out.println(filePath);
-        ArrayList listResult = e.importFile(filePath);
-        
-        for(int i=0; i<listResult.size(); i++){
-            m = (Mot)listResult.get(i);
-            for(int j=i+1; j<listResult.size(); j++){
-                if(m.testLien((Mot)listResult.get(j))){
-                    //Pour voir les liens se faire, décommentez la ligne ci-dessous : 
-                   // System.out.println(m.getValeur() + " proche de " + ((Mot)listResult.get(j)).getValeur());
-                }
             }
         }
         System.out.println("Debut Algo");
-        int nbcomposanteconnexe=0;
-        for(int i=0; i<listResult.size();i++){
-            System.out.println("Boucle : "+i);
-            m = (Mot)listResult.get(i);
-            if(!m.visite){
+        int nbcomposanteconnexe = 0;
+        for (int i = 0; i < listResult.size(); i++) {
+            System.out.println("Boucle : " + i);
+            m = (Mot) listResult.get(i);
+            if (!m.visite) {
                 m.parcoursComposanteConnexe();
                 nbcomposanteconnexe++;
-            System.out.println("composante connexe : "+nbcomposanteconnexe);
+                System.out.println("composante connexe : " + nbcomposanteconnexe);
             }
         }
-        System.out.println("Nombre de composantes connexe : "+nbcomposanteconnexe);
+        System.out.println("Nombre de composantes connexe : " + nbcomposanteconnexe);
+
     }
 }
