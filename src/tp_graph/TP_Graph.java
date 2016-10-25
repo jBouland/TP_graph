4380e4782e7931c996a8c0414a86c9c541da77d9
Epulapp <joris.bouland@gmail.com>
2016-10-25 15:09:57 +0200

    commit main

--- src/tp_graph/TP_Graph.java
+++ src/tp_graph/TP_Graph.java
@@ -5,6 +5,9 @@
  */
 package tp_graph;
 
+import java.io.File;
+import java.util.ArrayList;
+
 
 /**
  *
@@ -16,6 +19,10 @@ public class TP_Graph {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Engine e = new Engine();
+        String filePath = new File("").getAbsolutePath();
+        filePath = filePath.concat("\\mots04lettres.txt");
+        System.out.println(filePath);
+        ArrayList listResult = e.importFile(filePath);
     }
 }
